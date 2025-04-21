71e5463123646b24c9244b6bd3090604e805c879
Razan mohammed <razan.reza2022@gmail.com>
2025-04-22 01:26:36 +0200

    Update file.js

---
+++ file.js
@@ -0,0 +1,8 @@
+let loginform = document.querySelector('.login_form')
+
+function showform(){
+    loginform.classList.add("active");
+}
+function hideform(){
+    loginform.classList.remove("active");
+}
