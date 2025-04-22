bd32106cbdf5aff2ac4892171d0133304dc23829
rawanosama225 <osamarawan932@gmail.com>
2025-04-22 13:11:52 +0200

    update
    signup

--- file.js
+++ file.js
@@ -1,8 +1,74 @@
-let loginform = document.querySelector('.login_form')
+let loginform = document.querySelector('.login_form');
+let lognowbtn = document.querySelector('.loginbtn') 
+let signupform= document.querySelector('.signup_form');
 
+/*For login */
+
+/*function btzhr el login page lma ndos 3la el login button in nav bar suction */
 function showform(){
+    document.querySelector('.login_form form').reset();
     loginform.classList.add("active");
+    
 }
+
+/*function bt2fl el form lma ndos 3la zorar el submit bta3 el login + enha msh ht2fl 
+tol ma el inputs mfhsh 7aga + lma nktb informaion feha lma n3ml submit w nd5ol tany bttshal  */
+function hide() {
+    let email =document.querySelector('#email').value.trim();
+    let password =document.querySelector('#password').value.trim();
+    let remember =document.querySelector('#remember').checked;
+    if(email !== '' && password !== '' && remember) {  
+        loginform.classList.remove("active");  
+      } else {  
+        alert("Please complete all requirements");  
+      }  
+}
+
+/* bt2fl el form lma ndos 3la (x) */
 function hideform(){
     loginform.classList.remove("active");
+    document.querySelector('.login_form form').reset();
+    
 }
+/* end for login */
+
+
+
+/*For signup */
+
+
+/* when we click on zorar el sign up (el gmb don't have account) byodena l signup page 
+ w y2fl el login page like a switch*/
+function signupshow(){
+    loginform.classList.remove("active");
+    signupform.classList.add("active");
+}
+
+/* zorar el (x) */
+function signuphide(){
+    signupform.classList.remove("active");
+    document.querySelector('.signup_form form').reset();
+}
+
+
+/* submit signup */
+function hide_from_signup(){
+    let email =document.querySelector('#em').value.trim();
+    let password =document.querySelector('#pass').value.trim();
+    let username=document.querySelector('#username').value.trim();
+    let agree=document.querySelector('#agree').checked;
+
+    if(email !== '' && password !== '' && username !== '' && agree) {  
+        signupform.classList.remove("active");
+        document.querySelector('.signup_form form').reset();
+        return true;
+      } else {  
+        alert("Please provide the following to verify your identity"); 
+        return false;
+      }  
+      
+}
+/* end For sign up  */
+
+
+
