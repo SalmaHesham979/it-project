55ac31bf21d88e0e5dd5dd594c9180d2589737ad
rawanosama225 <osamarawan932@gmail.com>
2025-05-07 18:31:23 +0300

    reserve recovery
    ra

--- file.js
+++ file.js
@@ -151,13 +151,29 @@ ScrollReveal().reveal(".discover__card", {
 
 /*reserve button*/
 function showfeedback() {
-  document.getElementById("modal").style.display = "block";
-  document.getElementById("showButton").style.display = "none";
+  let to = document.querySelector ('.to').value.trim();
+  let from=document.querySelector('.from').value.trim();
+  let date = document.querySelector('.date').value.trim();
+  let classes = document.querySelector ('.class').value.trim();
+  if (to !=='' && from !=='' && date && classes!=='') 
+  {
+    document.getElementById("modal").style.display = "block";
+    document.getElementById("showButton").style.display = "none";
+    document.body.classList.add('no-scroll');
+
+  }
+else {
+  alert ('complete all required before submit');
+}
+ 
 }
 
 function closeModal() {
   document.getElementById("modal").style.display = "none";
   document.getElementById("showButton").style.display = "block";
+  document.body.classList.remove('no-scroll');
+  document.getElementById('reserveform').reset();
+
 }
 //the end 
 // zorar el send
